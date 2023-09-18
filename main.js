796731bb113ea1d4fd73d1f3d66bebd9b3a96f47
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2023-09-18 13:56:30 -0400

    Replace Files

--- main.js
+++ main.js
@@ -1,73 +1,26 @@
-import { default as seagulls } from './seagulls.js'
-import { default as Video    } from './video.js'
-import { default as Audio    } from './audio.js'
+import { default as seagulls } from '../../seagulls.js'
 
-import { Pane } from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.1/dist/tweakpane.min.js';
+const sg      = await seagulls.init(),
+      frag    = await seagulls.import( './frag.wgsl' ),
+      compute = await seagulls.import( './compute.wgsl' ),
+      render  = seagulls.constants.vertex + frag,
+      size    = window.innerWidth * window.innerHeight,
+      state   = new Float32Array( size )
 
-var mouseX = null;
-var mouseY = null;
-var mouseDown = null;
-    
-document.addEventListener('mousemove', onMouseUpdate, false);
-document.addEventListener('mouseenter', onMouseUpdate, false);
-document.body.onmousedown = (e) => {mouseDown = 1;};
-document.body.onmouseup = (e) => {mouseDown = 0;};
-
-function onMouseUpdate(event) {
-  mouseX = event.pageX;
-  mouseY = event.pageY;
+for( let i = 0; i < size; i++ ) {
+  state[ i ] = Math.round( Math.random() )
 }
 
-async function main() {
-  // Variables
-  let time = 0;
-  const params = { timeScale: 1, camMixer: .95, camColor: .75, feedback: .8, mouseColor: { r:1, g:0, b:0  } }
-  
-  // Tweakpane
-  const pane = new Pane();
-  pane
-    .addBinding(params, 'timeScale', { min: 0, max: 2 })
-    .on('change',  e => { params.timeScale = e.value; })
-  pane
-    .addBinding(params, 'camMixer', { min: 0, max: 1 })
-    .on('change',  e => { sg.uniforms.camMixer = e.value; })
-  pane
-    .addBinding(params, 'camColor', { min: 0, max: 1 })
-    .on('change',  e => { sg.uniforms.camColor = e.value; })
-  pane
-    .addBinding(params, 'feedback', { min: 0, max: 1 })
-    .on('change',  e => { sg.uniforms.feedback = e.value; })
-  pane
-    .addBinding(params, 'mouseColor', { color: { type:'float' } })
-    .on('change',  e => { sg.uniforms.mouseColor = Object.values(e.value); })
-  
-  // Initialization
-  const sg = await seagulls.init()
-  const frag = await seagulls.import( './frag.wgsl' );
-  document.body.onclick = Audio.start
-  await Video.init()
-
-  // Seagulls
-  sg.uniforms({ 
-    time:0, 
-    res:[window.innerWidth, window.innerHeight],
-    audio:[0,0,0],
-    mouse:[0,0,0],
-    camMixer: params.camMixer,
-    camColor: params.camColor,
-    feedback: params.feedback,
-    mouseColor: Object.values( params.mouseColor )
-  })
-  .onframe( ()=> {
-    time += params.timeScale / 10;
-    
-    sg.uniforms.time = time;
-    sg.uniforms.audio = [ Audio.low, Audio.mid, Audio.high ];
-    sg.uniforms.mouse = [ mouseX, mouseY, mouseDown];
-  })
-  .textures([ Video.element ]) 
-  .render( frag )
+//window.addEventListener( 'load', function() {
+sg.buffers({ stateA:state, stateB:state })
+  .uniforms({ resolution:[ window.innerWidth, window.innerHeight ] })
+  .backbuffer( false )
+  .pingpong( 1 )
+  .compute( 
+    compute, 
+    [Math.round(window.innerWidth / 8), Math.round(window.innerHeight/8), 1], 
+    { pingpong:['stateA'] } 
+  )
+  .render( render )
   .run()
-}
 
-main()
